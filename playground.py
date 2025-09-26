bc72e6ec654a9003485ba120c2bcd56620580c08
Reversal Destiny Studio <133374491+AnodHuang@users.noreply.github.com>
2025-09-26 21:55:16 +0800

    color

--- playground.py
+++ playground.py
@@ -22,7 +22,7 @@ pygame.init()
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-manager = gui.UIManager((screen.get_size()), starting_language='zh', theme_path='theme.json',
+manager = gui.UIManager((screen.get_size()), starting_language='zh', theme_path='in_game_theme.json',
                                      translation_directory_paths=['languages'])
 
 ui = PlayScreen(screen, manager)
